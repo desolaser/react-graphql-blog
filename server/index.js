1590aa9824dd6c49d26d4b6ab391d3b481e69c55
Felipe Olavarria <178.felipe@gmail.com>
2020-04-27 11:56:10 -0400

    Add schema to index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
 
 const app = express()
 
 // Handles graphql requests
 app.use('/graphql', graphqlHTTP({
-    
+    schema
 }))
 
 app.listen(4000, () => console.log('Server running on port 4000'))
