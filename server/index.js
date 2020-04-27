11b8b7914828c9f89baa2b5751dd9d5c078b813e
Felipe Olavarria <178.felipe@gmail.com>
2020-04-27 11:41:04 -0400

    Add graphql express to index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
+
 const app = express()
+
+// Handles graphql requests
+app.use('/graphql', graphqlHTTP({
+    
+}))
 
 app.listen(4000, () => console.log('Server running on port 4000'))
