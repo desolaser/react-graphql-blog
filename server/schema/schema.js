d26ca242f6c68e7cf2085506ad1eebf825fb0aee
Felipe Olavarria <178.felipe@gmail.com>
2020-04-27 12:04:32 -0400

    Add lodash and resolve function

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,16 @@
 const graphql = require('graphql')
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql
 
+// Utilities for array operations
+const _ = require('lodash')
+
+// Dummy data
+const posts = [
+    {id: 'post-1', title: 'Post 1', text: 'Post 1 text'},
+    {id: 'post-2', title: 'Post 2', text: 'Post 2 text'},
+    {id: 'post-3', title: 'Post 3', text: 'Post 3 text'},
+]
+
 const PostType = new GraphQLObjectType({
     name: 'Post',
     fields: () => ({
@@ -17,12 +27,14 @@ const RootType = new GraphQLObjectType({
             type: PostType,
             args: { id: { type: GraphQLString } },
             resolve(parents, args) {
-
+                // We get the data from the mongoDB database here
+                return _.find(posts, {id: args.id})
             }
         }
     }
 })
 
+// Export our RootType query
 module.exports = new GraphQLSchema({
     query: RootType
 })
