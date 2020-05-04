9b164ca4c35ca8fa8ec9a1318f2dcac5ba2f719b
Felipe Olavarria <178.felipe@gmail.com>
2020-05-04 15:56:36 -0400

    Add comment type and relationship between post and comment

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -16,12 +16,33 @@ const posts = [
     {id: 'post-3', title: 'Post 3', text: 'Post 3 text'},
 ]
 
+const comments = [
+    {id: 'comment-1', text: 'Comment 1', postId: 'post-1'},
+    {id: 'comment-2', text: 'Comment 2', postId: 'post-2'},
+    {id: 'comment-3', text: 'Comment 3', postId: 'post-3'},
+]
+
 const PostType = new GraphQLObjectType({
     name: 'Post',
     fields: () => ({
         id: { type: GraphQLID },
         title: { type: GraphQLString },
         text: { type: GraphQLString }
+    })
+})
+
+const CommentType = new GraphQLObjectType({
+    name: 'Comment',
+    fields: () => ({
+        id: { type: GraphQLID },
+        text: { type: GraphQLString },
+        post: {
+            type: PostType,
+            resolve(parent, args) {
+                console.log(parent)
+                return _.find(posts, {id: parent.postId})
+            }
+        }
     })
 })
 
@@ -34,6 +55,13 @@ const RootType = new GraphQLObjectType({
             resolve(parents, args) {
                 // We get the data from the mongoDB database here
                 return _.find(posts, {id: args.id})
+            }
+        },
+        comment: {
+            type: CommentType,
+            args: { id: { type: GraphQLID } },
+            resolve(parents, args) {
+                return _.find(comments, {id: args.id})
             }
         }
     }
