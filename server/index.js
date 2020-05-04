5a120a8eb30b83c8b8ed8b04321a6b571d728c10
Felipe Olavarria <178.felipe@gmail.com>
2020-05-04 16:33:33 -0400

    Add mongoose connection with database and new comments

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const mongoose = require('mongoose')
+
 const schema = require('./schema/schema')
 
 const app = express()
 
+mongoose.connect('mongodb+srv://admin:666334ss@mongotest-cvjig.gcp.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connection.once('open', () => console.log('Connected to database'))
+
 // Handles graphql requests
 app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
+    schema,  // passes the schema
+    graphiql: true // activates graphiql
 }))
 
 app.listen(4000, () => console.log('Server running on port 4000'))
