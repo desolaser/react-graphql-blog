651faa74cfb2c5d517e7b06c3220f0894272b6f3
Felipe Olavarria <178.felipe@gmail.com>
2020-05-05 15:13:45 -0400

    Add apollo client and provider to App js

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+ 
 import PostList from './components/PostList'
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql'
+})
 
 const App = () => {
     return (
-        <div className="container">
-            <header className="header">
-                React GraphQL Blog
-            </header>
-            <main className="main">
-                <PostList />
-            </main>
-            <footer className="footer">
-                Footer
-            </footer>
-        </div>
+        <ApolloProvider client={client}>
+            <div className="container">
+                <header className="header">
+                    React GraphQL Blog
+                </header>
+                <main className="main">
+                    <PostList />
+                </main>
+                <footer className="footer">
+                    Footer
+                </footer>
+            </div>
+        </ApolloProvider>
     );
 }
 
