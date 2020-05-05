790a940a9d8e99717eed3330ecc550c1943dfc65
Felipe Olavarria <178.felipe@gmail.com>
2020-05-05 14:48:20 -0400

    Add postList to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import PostList from './components/PostList'
 
 const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                
+        <div className="container">
+            <header className="header">
+                React GraphQL Blog
             </header>
+            <main className="main">
+                <PostList />
+            </main>
+            <footer className="footer">
+                Footer
+            </footer>
         </div>
     );
 }
