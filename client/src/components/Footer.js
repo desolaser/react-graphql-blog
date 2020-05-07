55b154e538be6ffddc9fc11a5d52b48e61c857c0
Felipe Olavarria <178.felipe@gmail.com>
2020-05-07 19:53:27 -0400

    Fix header and navbar because they were inverted

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <header className="header">
-            React GraphQL Blog
-        </header>
+        <footer className="footer">
+            Footer
+        </footer>
     )
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <footer className="footer">
-            Footer
-        </footer>
+        <header className="header">
+            React GraphQL Blog
+        </header>
     )
 }
 
