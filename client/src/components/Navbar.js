8094fe8d4373e1946c3f131a716e2d253516d55b
Felipe Olavarria <178.felipe@gmail.com>
2020-05-08 16:39:32 -0400

    Add search bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,64 +1,98 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { makeStyles } from '@material-ui/core/styles'
+import { fade, makeStyles } from '@material-ui/core/styles'
+import SearchIcon from '@material-ui/icons/Search'
 import { 
     AppBar,
     Toolbar,
-    IconButton,
     Typography,
-    MenuItem,
-    Menu,
-    Fade
+    Button,
+    InputBase
 } from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu'
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
+        flexGrow: 1,
     },
     menuButton: {
-      marginRight: theme.spacing(2),
+        marginRight: theme.spacing(2),
     },
     title: {
-      flexGrow: 1,
+        flexGrow: 1,
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
     },
-}));
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '12ch',
+            '&:focus': {
+            width: '20ch',
+            },
+        },
+    },
+  }));
   
 const Header = () => {
     const classes = useStyles();
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
     return (
         <AppBar position="static">
             <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon onClick={handleClick}/>
-                    <Menu
-                        id="fade-menu"
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={open}
-                        onClose={handleClose}
-                        TransitionComponent={Fade}
-                    >
-                        <MenuItem component={Link} to="/" onClick={handleClose}>Home</MenuItem>
-                        <MenuItem component={Link} to="/post" onClick={handleClose}>Post</MenuItem>
-                        <MenuItem component={Link} to="/comment" onClick={handleClose}>Comment</MenuItem>
-                    </Menu>
-                </IconButton>
                 <Typography variant="h6" className={classes.title}>
-                    Forum
+                    React GraphQL Forum
                 </Typography>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon />
+                    </div>
+                    <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </div>
+                <Button variant="contained" color="primary">
+                    Sign in
+                </Button>
+                <Button variant="contained" color="primary">
+                    Login
+                </Button>
             </Toolbar>
         </AppBar>        
     )
