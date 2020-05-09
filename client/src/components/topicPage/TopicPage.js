156ce3069e1a1d4b18015bcf3b001616e05818c3
Felipe Olavarria <178.felipe@gmail.com>
2020-05-09 13:40:23 -0400

    Add posts to TopicPage

--- client/src/components/topicPage/TopicPage.js
+++ client/src/components/topicPage/TopicPage.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { AppBar, Typography, List, ListItem, ListItemText, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 
-const TopicPage = () => {
+const useStyles = makeStyles({
+    root: {
+        margin: '15px 0',
+        '& .app-bar': {
+            padding: '4px 10px',
+            backgroundColor: '#4CB58A'
+        },
+        '& .topic-item': {
+            backgroundColor: '#FFFFFF',
+            marginBottom: '5px',
+        }
+    }
+})
+
+const GET_TOPIC = gql`
+    query getTopic($id: ID!) {
+        topic(id: $id) {
+            id
+            name
+            posts {
+                id
+                title
+                user {
+                    id
+                    name
+                }
+            }
+            user {
+                id
+                name
+            }
+        }
+    }
+`
+
+const TopicPage = props => {    
+    const classes = useStyles()
+
+    const { loading, error, data } = useQuery(GET_TOPIC, {
+        variables: { id: props.match.params.id }
+    })
+
+    if (loading) return "loading..."
+    if (error) return `Error ${error.message}`
+
+    console.log(data)
+
     return (
-        <div>
-            
+        <div className={classes.root}>
+            <AppBar position="static" className="app-bar">
+                <Grid container justify="space-between">
+                    <Typography variant="subtitle1" inline>
+                        {data.topic.name}
+                    </Typography>
+                    <Typography variant="caption" align="right" inline>
+                        {`Created by ${data.topic.user.name}`}
+                    </Typography>
+                </Grid>
+            </AppBar>
+            <List component="nav">
+                {data.topic.posts.map(post => (
+                    <ListItem key={post.id} className="topic-item" component="a" href={`/post/${post.id}`} button>
+                        <ListItemText primary={post.title} secondary={`Created by ${post.user.name}`} />
+                    </ListItem>
+                ))}
+            </List>
         </div>
     )
 }
