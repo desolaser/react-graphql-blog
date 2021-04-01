ae79893f1f0ab7b3ccf455fa893d6bdea22b4dc3
Felipe Olavarria <178.felipe@gmail.com>
2021-04-01 17:31:02 -0300

    Add Login and Sign In forms and styles

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React from 'react'
+import { Card, CardHeader, CardContent, TextField, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+  root: {
+    padding: 20,
+    textAlign: 'center',
+  },
+  input: {
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  button: {
+    marginTop: 20,
+  }
+})
 
 const Login = () => {
+  const classes = useStyles()
   return (
-    <div>
-      Login
-    </div>
+    <Card className={classes.root}>
+      <CardHeader title="Login" />
+      <CardContent>
+        <TextField 
+          className={classes.input}
+          label="Name"
+          placeholder="Username"
+        />
+        <TextField 
+          className={classes.input}
+          label="Password"
+          placeholder="Your Password"
+        />
+      </CardContent>
+      <Button className={classes.button} variant="contained" color="primary">
+        Login
+      </Button>
+    </Card>
   )
 }
 
