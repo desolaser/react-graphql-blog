073467029b528112096f40f83fc76a531cfd5763
Felipe Olavarria <178.felipe@gmail.com>
2021-04-05 11:19:38 -0400

    Add EditDeleteButtons component
    - Add EditDeleteButtons component in mainPage's Topic component to
    replace edit and delete buttons

--- client/src/pages/mainPage/Topic.js
+++ client/src/pages/mainPage/Topic.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { ListItem, ListItemText, IconButton, Grid } from '@material-ui/core'
+import { ListItem, ListItemText, Grid } from '@material-ui/core'
 import dateFormatter from '../../utils/dateFormatter'
-import DeleteIcon from '@material-ui/icons/Delete'
-import EditIcon from '@material-ui/icons/Edit'
+import EditDeleteButtons from '../../components/EditDeleteButtons'
 
 const Topic = ({ topic }) => {
   const formattedDate = dateFormatter(topic.createdAt)
@@ -19,26 +18,15 @@ const Topic = ({ topic }) => {
     <Grid container>
       <Grid item xs={10}>
         <ListItem key={topic.id} className="topic-item" component="a" href={`/topic/${topic.id}`} button>
-          <ListItemText primary={topic.name} secondary={`Created by ${topic.user.name} - ${formattedDate}`} />
+          <ListItemText
+            key={topic.id}
+            primary={topic.name}
+            secondary={`Created by ${topic.user.name} - ${formattedDate}`}
+          />
         </ListItem>
       </Grid>
       <Grid item xs={2}>
-        <ListItem key={topic.id} className="topic-item" style={{ height: "100%" }}>
-          <IconButton 
-            variant="contained" 
-            onClick={deleteTopic} 
-            style={{ margin: "auto" }}
-            color="secondary">
-            <DeleteIcon />
-          </IconButton>
-          <IconButton 
-            variant="contained" 
-            onClick={editTopic} 
-            style={{ margin: "auto" }}
-            color="primary">
-            <EditIcon />
-          </IconButton>
-        </ListItem>
+        <EditDeleteButtons editFunction={editTopic} deleteFunction={deleteTopic} />
       </Grid>
     </Grid>
   )
