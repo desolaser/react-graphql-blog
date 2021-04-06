efdcc82b2262f5ea73ffa09df5f9a07603948be6
Felipe Olavarria <178.felipe@gmail.com>
2021-04-06 13:44:20 -0400

    Add .env and express-jwt middleware to the server in index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
+require('dotenv').config({path: './env'})
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const jwt = require('express-jwt')
 
 const schema = require('./schema/schema')
 
 const app = express()
+
+const auth = jwt({
+    secret: '1221eqwdkjkdl',
+    algorithms: ['RS256'],
+    credentialsRequired: false
+})
 
 app.use(cors())
 
@@ -13,9 +21,13 @@ mongoose.connect('mongodb+srv://admin:666334ss@mongotest-cvjig.gcp.mongodb.net/t
 mongoose.connection.once('open', () => console.log('Connected to database'))
 
 // Handles graphql requests
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', auth, graphqlHTTP(req => ({
     schema,  // passes the schema
-    graphiql: true // activates graphiql
+    graphiql: true, // activates graphiql
+    context: {
+        user: req.user
+    }
 }))
+)
 
 app.listen(4000, () => console.log('Server running on port 4000'))
