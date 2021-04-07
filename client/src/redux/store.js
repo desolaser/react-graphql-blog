24578b441ea59193519bf3a960d4e61b88540ff8
Felipe Olavarria <178.felipe@gmail.com>
2021-04-07 08:46:44 -0400

    Update auth reducer import

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './reducers/AuthReducer'
+import authReducer from './reducers/authSlice'
 
 const store = configureStore({
   reducer: {
