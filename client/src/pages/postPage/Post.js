cb074099a796b98f37d0983142150dc90bb95512
Felipe Olavarria <178.felipe@gmail.com>
2022-03-22 15:42:50 -0300

    Add responsive sizes to Post

--- client/src/pages/postPage/Post.js
+++ client/src/pages/postPage/Post.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import { Grid, Typography, Card, CardHeader, CardContent } from '@material-ui/core'
+import { 
+  Grid, 
+  Typography, 
+  Card, 
+  CardHeader, 
+  CardContent 
+} from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
-
+import { Remarkable } from 'remarkable'
 import dateFormatter from '../../utils/dateFormatter'
+
+const md = new Remarkable()
 
 const useStyles = makeStyles({
   card: {
@@ -10,13 +18,13 @@ const useStyles = makeStyles({
   }
 })
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   const classes = useStyles()
   const formattedDate = dateFormatter(post.createdAt)
 
   return (
     <Grid container direction={"row"} spacing={3}>
-      <Grid item xs={3}>
+      <Grid item xs={12} md={3}>
         <Card className={classes.card}>
           <CardContent>
             <Typography variant="h6">
@@ -28,15 +36,13 @@ const Post = ({post}) => {
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={9}>
+      <Grid item xs={12} md={9}>
         <Card className={classes.card}>
           <CardHeader
             title={post.title}
           />
           <CardContent>
-            <Typography color="textSecondary">
-              {post.content}
-            </Typography>
+            <Typography dangerouslySetInnerHTML={{__html: md.render(post.content)}} />
             <Typography mt={4} color="textSecondary">
               {formattedDate}
             </Typography>
