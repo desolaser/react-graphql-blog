ee9ae9ccb0dedd344552d2d5c9c1e7161ebdc0b3
Felipe Olavarria <178.felipe@gmail.com>
2022-03-22 15:37:37 -0300

    Use Typography instead of Box to show preview in PostForm

--- client/src/pages/addPost/PostForm.js
+++ client/src/pages/addPost/PostForm.js
@@ -74,8 +74,8 @@ const PostForm = ({title, setTitle, topicId, setTopicId, content, setContent, ha
         />
       </FormControl>
       <Typography mt={2} variant="h5" mb={2}>Preview</Typography>
-      <Box my={2} dangerouslySetInnerHTML={{__html:md.render(content)}}>
-      </Box>
+      <Typography my={2} dangerouslySetInnerHTML={{__html: md.render(content)}}>
+      </Typography>
       <Button 
         variant="contained" 
         color="primary" 
